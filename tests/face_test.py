90266a0eca8e57bfa779960ce202b19a192f27b1
Timoctrl <timostle@gmail.com>
2026-01-11 19:17:40 +0100

    face fix

--- tests/face_test.py
+++ tests/face_test.py
@@ -1,22 +1,20 @@
 import time
 from RPLCD.i2c import CharLCD
 
-lcd = CharLCD(i2c_addr=0x27, num_cols=16, num_rows=2)  # Your address!
+lcd = CharLCD('PCF8574', 0x27, 2, cols=16, rows=2)  # Correct for 1602A backpack!
 
 lcd.clear()
 lcd.write_string("Hello!")
-lcd.cursor_pos = (0, 1)
+lcd.cursor_pos = (1, 0)
 lcd.write_string("I'm Snowy :)")
 time.sleep(5)
 
 lcd.clear()
+moods = ["HAPPY :)", "SLEEPY ZzZ", "CURIOUS ??"]
 while True:
-    lcd.write_string("Snowy Mood:    ")
-    lcd.cursor_pos = (0, 1)
-    moods = ["HAPPY :)", "SLEEPY ZzZ", "CURIOUS ??"]
     for mood in moods:
+        lcd.clear()
+        lcd.write_string("Snowy Mood:")
+        lcd.cursor_pos = (1, 0)
         lcd.write_string(mood)
         time.sleep(3)
-    lcd.clear()
-    
-
